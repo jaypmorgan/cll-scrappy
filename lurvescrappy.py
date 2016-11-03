635a876353b457afa675d11344badf108194c46a
jaypmorgan <jaymiles17@gmail.com>
2016-11-03 19:25:07 +0000

    Completed basic get all feature, returns list of a clls used

--- lurvescrappy.py
+++ lurvescrappy.py
@@ -20,7 +20,7 @@ parser.add_argument('--all', dest='getall', help='get all the command line loves
 args = parser.parse_args()
 
 #CLLs
-clls = {}
+clls = []
 
 def get_source():
 	hdr= {
@@ -31,22 +31,26 @@ def get_source():
        	'Accept-Language': 'en-US,en;q=0.8',
        	'Connection': 'keep-alive'
 	}
-	source = urllib2.Request('http://ubuntupodcast.org/feed/', headers=hdr)
+	source = urllib2.Request('http://ubuntupodcast.org/feed/podcast', headers=hdr)
 	source = urllib2.urlopen(source)
 	return source.read()
 
 # function for getting all the CLLs from the website
 # should be normally used when there is a back log
 def get_all():
-	target_date = date_now
-	date_list = []
+	root = lxml.etree.fromstring(get_source())
+	code = root.xpath('*//content:encoded', namespaces={
+		'content':'http://purl.org/rss/1.0/modules/content/',
+	})
 
-	# we want to generate a list of URLs to crawl. We start with the current year
-	# and month and work backwards to the oldest date (set in the config file)
-	while(target_date > oldest_archive):
-		url_string = str(target_date.year) + '/' + target_date.strftime('%m') + '/' # the url format is '/year/month/'
-		date_list.append(url_string) # add this to the array of urls to be scrapped
-		target_date = target_date - dateutil.relativedelta.relativedelta(months = 1) # take away another month
+	for x in range(0,len(code)):
+		cll = html.fromstring(code[x].text)
+		cll = cll.xpath('*//code')
+		try:
+			clls.append(cll[0].text) # TODO - what if there are multiples in episode???
+		except IndexError:
+			print()
+	return clls
 
 # a function for getting only the latest CLL and outputs
 # a singular element that can be ammended to an existing
@@ -56,9 +60,10 @@ def get_latest():
 	code = root.xpath('*//content:encoded', namespaces={
 		'content':'http://purl.org/rss/1.0/modules/content/',
 	})
-	cll = html.fromstring(code[1].text)
+	cll = html.fromstring(code[0].text)
 	cll = cll.xpath('*//code')
-	print(cll[0].text)
+	clls.append(cll[0].text)
+	return clls
 
 # if you have a date of a particular podcast (or range of
 # dates) then this function will iterate over that range and
@@ -66,4 +71,4 @@ def get_latest():
 def get_particular(url):
 	return
 
-get_latest()
+print(get_all())
