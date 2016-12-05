67a568be7032fdd6895849ceccc267ebf5ccf076
jaypmorgan <jaymiles17@gmail.com>
2016-12-05 20:30:28 +0000

    [Fix: #3] Implemented script into website logic.
    I have implemented the lurvescrappy script into the website by setting up a cronjob to run the script and keep a local json file up to date. When the website is loaded it will read from data.json and append the table rows to the table.

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	$('#command-table').dataTable();
+	$.getJSON('../data.json', function(json) {
+		output_html = "";
+		$.each(json, function(key,value) {
+			output_html += "<tr>";
+			$.each(value, function(key,value) {
+				output_html += "<td>";
+				output_html += value;
+				output_html += "</td>";
+			});
+			output_html += "</tr>";
+		});
+		$('tbody').append(output_html);
+		$('#command-table').dataTable();
+	});
+
 });
