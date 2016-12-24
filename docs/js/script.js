94e65149aecf7eec9e745cc7e221cfd3da1ae2f6
jaypmorgan <jaymiles17@gmail.com>
2016-12-24 13:12:47 +0000

    Adding URL into appearance title

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,15 +4,23 @@ $(document).ready(function() {
 		$.each(json, function(key,value) {
 			output_html += "<tr>";
 			$.each(value, function(key,value) {
-				output_html += "<td>";
-				output_html += value;
-				output_html += "</td>";
+				if(key=='url') {
+					url = value;
+				} else if(key=='title') {
+					output_html += '<td><a href="' + url + '">';
+					output_html += value;
+					output_html += '</a></td>';
+
+				} else {
+					output_html += '<td>'+value[0]+'</td>';
+				}
 			});
 			output_html += "</tr>";
 		});
 		$('tbody').append(output_html);
 		$('#command-table').dataTable({
-			"order": [[ 1, "desc" ]]
+			"order": [[ 1, "desc" ]],
+			"iDisplayLength" : 25
 		});
 	});
 
